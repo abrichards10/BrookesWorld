dc1902d3d81faff3895173a1ba850b118c7991bf
abrichards10 <angelarichards261@gmail.com>
2024-08-08 18:40:18 -0700

    fixed carousel

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -11,80 +11,46 @@ import pic2 from "./assets/Witches2.png";
 import career1 from "./assets/Career_Chatter1.png";
 import career2 from "./assets/Career_Chatter2.png";
 import career3 from "./assets/Career_Chatter3.png";
+import Magnifier from "react-magnifier";
 
 const CarouselComponent = () => {
   return (
     <div className="carousel-container">
       <h3 className="titles-app">Mobile App Design and Development</h3>
-      <Carousel controls={false}>
-        <Carousel.Item className="carousel-item-custom">
-          <Row className="d-flex justify-content-center align-items-center h-100">
+      <Carousel>
+        <Carousel.Item>
+          <Row>
             <Col xs={1} md={4} className="d-flex justify-content-center">
-              <img
-                className="d-block iphone-image"
-                src={gif1} // Import GIF from assets
-                alt="First slide"
-              />
+              <Magnifier src={gif1} />
             </Col>
             <Col xs={12} md={4} className="d-flex justify-content-center">
-              <img
-                className="d-block iphone-image"
-                src={gif2} // Import GIF from assets
-                alt="First slide"
-              />
+              <Magnifier src={gif2} width={500} />
             </Col>
             <Col xs={12} md={4} className="d-flex justify-content-center">
-              <img
-                className="d-block iphone-image"
-                src={gif3} // Import GIF from assets
-                alt="First slide"
-              />
-            </Col>
-          </Row>
-          {/* <Carousel.Caption>
-            <h3>Pocket Python</h3>
-          </Carousel.Caption> */}
-        </Carousel.Item>
-        <Carousel.Item className="carousel-item-custom">
-          <Row className="d-flex justify-content-center align-items-center h-100">
-            <Col xs={12} md={4} className="d-flex justify-content-center">
-              <img
-                className="d-block iphone-image"
-                src={pic1} // Import GIF from assets
-                alt="Second slide"
-              />
-            </Col>
-            <Col xs={12} md={4} className="d-flex justify-content-center">
-              <img
-                className="d-block iphone-image"
-                src={pic2} // Import GIF from assets
-                alt="Second slide"
-              />
+              <Magnifier src={gif3} width={500} />
             </Col>
           </Row>
         </Carousel.Item>
-        <Carousel.Item className="carousel-item-custom">
+        <Carousel.Item>
           <Row className="d-flex justify-content-center align-items-center h-100">
             <Col xs={12} md={4} className="d-flex justify-content-center">
-              <img
-                className="d-block iphone-image"
-                src={career1} // Import GIF from assets
-                alt="Third slide"
-              />
+              <Magnifier src={pic1} width={500} />
             </Col>
             <Col xs={12} md={4} className="d-flex justify-content-center">
-              <img
-                className="d-block iphone-image"
-                src={career2} // Import GIF from assets
-                alt="Third slide"
-              />
+              <Magnifier src={pic2} width={500} />
+            </Col>
+          </Row>
+        </Carousel.Item>
+        <Carousel.Item>
+          <Row>
+            <Col xs={12} md={4} className="d-flex justify-content-center">
+              <Magnifier src={career1} width={500} />
             </Col>
             <Col xs={12} md={4} className="d-flex justify-content-center">
-              <img
-                className="d-block iphone-image"
-                src={career3} // Import GIF from assets
-                alt="Third slide"
-              />
+              <Magnifier src={career2} width={500} />
+            </Col>
+            <Col xs={12} md={4} className="d-flex justify-content-center">
+              <Magnifier src={career3} width={500} />
             </Col>
           </Row>
         </Carousel.Item>
